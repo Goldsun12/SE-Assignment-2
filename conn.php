9f1fb011a547b07d4bc03d5acd697cbbeb43d2c1
sunxuan <sunxuan1212@gmail.com>
2017-03-11 23:21:20 +0800

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,9 @@
+<?php
+$db_user = "root";
+$db_pass = "";
+$db_host = "127.0.0.1";
+$db_port = 3306;
+$db_name = "jike_chat";
+
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);	
+?>
